a6cdfef9900d67701bf128f4eae98b2e408ff324
JonaStef <38321403+JonaStef@users.noreply.github.com>
2018-04-12 17:12:51 +0200

    Update cells.py

--- cells.py
+++ cells.py
@@ -6,7 +6,7 @@ import os
 import os.path
 
 def main():
-    search_fiji.fiji_path = 'ffiji-win64\Fiji.app\ImageJ-win64.exe'
+    #search_fiji.fiji_path ="fiji-win64\Fiji.app\ImageJ-win64.exe"
     image.image_filename = ''
     data.data_filename = ''
     classifier.classifier_filename = ''
@@ -77,21 +77,30 @@ def image():
 
 def launch_fiji():
     os.system(search_fiji.fiji_path + ' -macro files\cells_macro.ijm')
+    os.startfile( image.image_filename + "_ROI.tif")
 def search_fiji():
+    """
     if os.path.isfile(search_fiji.fiji_path):
     	launch_fiji()
     else:
-    	showerror('Cells', "Fiji n'a pas été trouvé, veuillez indiquer son chemin")
+    	showerror('Cells', "Fiji n'a pas été trouvé, veuillez le localiser")
     	search_fiji.fiji_path = askopenfilename(title="Fiji", filetypes=[('Fichier EXE', '.exe'), ('tous les fichiers', '.*')])
-    	launch_fiji()
+
+    """
+    showinfo('Cells', "Veuillez localiser Fiji")
+    search_fiji.fiji_path = askopenfilename(title="Fiji", filetypes=[('Fichier EXE', '.exe'), ('tous les fichiers', '.*')])
+    showinfo('Cells', "Ce precessus peut prendre du temps... Ne paniquez pas")
+    launch_fiji()
+
 
 
 def run():
 
     image_name_list = image.image_filename.split('/')
     image_name = image_name_list[-1]
+    image_no_file_exttention = image_name[:-4]
 
-    macro = "open('" + image.image_filename + "');run('Duplicate...', ' ');selectWindow('" + image_name + "');run('Trainable Weka Segmentation');selectWindow('Trainable Weka Segmentation v3.2.23');call('trainableSegmentation.Weka_Segmentation.loadClassifier', '" + classifier.classifier_filename + "');wait(1000);call('trainableSegmentation.Weka_Segmentation.loadData', '" + data.data_filename + "');wait(1000);call('trainableSegmentation.Weka_Segmentation.getProbability');wait(1000);selectWindow('Probability maps');setThreshold(0.99, 1.0000);setOption('BlackBackground', true);run('Convert to Mask', 'method=Default background=Dark calculate black');run('Watershed', 'slice');run('Set Measurements...', 'area perimeter bounding shape redirect=None decimal=3');run('Analyze Particles...', 'size=5-Infinity show=Outlines display exclude clear add in_situ slice');selectWindow('image-1.tif');roiManager('Show All');run('Flatten');run('Save', 'save=[" + image.image_filename + "_ROI.tif]');"
+    macro = "open('" + image.image_filename + "');run('Duplicate...', ' ');selectWindow('" + image_name + "');run('Trainable Weka Segmentation');wait(10000);selectWindow('Trainable Weka Segmentation v3.2.23');wait(10000);call('trainableSegmentation.Weka_Segmentation.loadClassifier', '" + classifier.classifier_filename + "');wait(15000);call('trainableSegmentation.Weka_Segmentation.loadData', '" + data.data_filename + "');wait(15000);call('trainableSegmentation.Weka_Segmentation.getProbability');wait(15000);selectWindow('Probability maps');setThreshold(0.99, 1.0000);setOption('BlackBackground', true);run('Convert to Mask', 'method=Default background=Dark calculate black');run('Watershed', 'slice');run('Set Measurements...', 'area perimeter bounding shape redirect=None decimal=3');run('Analyze Particles...', 'size=5-Infinity show=Outlines display exclude clear add in_situ slice');selectWindow('" + image_no_file_exttention + "-1.tif');roiManager('Show All');run('Flatten');run('Save', 'save=[" + image.image_filename + "_ROI.tif]');selectImage(nImages);close();run('Quit');"
 
     if classifier.classifier_filename == '' or image.image_filename == '' or data.data_filename == '':
         showerror('Cells', "Vous avez oublié de sélécionner une data ou un classifier ou une image")
